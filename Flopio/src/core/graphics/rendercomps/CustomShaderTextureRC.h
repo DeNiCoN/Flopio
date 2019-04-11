e17cde10cb426d2b5df17b705d3e588c81515028
DeNiCoN <denicon.yu@mail.ru>
2019-04-11 10:39:56 +0300

    Changed indices to be CCW

--- Flopio/src/core/graphics/rendercomps/CustomShaderTextureRC.h
+++ Flopio/src/core/graphics/rendercomps/CustomShaderTextureRC.h
@@ -18,8 +18,8 @@ namespace engine
 		0.5f, 0.5f };
 
 		static constexpr unsigned int indices[] = {
-		0, 1, 3,
-		0, 3, 2 };
+		3, 1, 0,
+		2, 3, 0 };
 	public:
 		Resource * texture;
 		unsigned int shaderProgramId;
